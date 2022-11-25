e50ad53950023174fa03efad47e18ff5b5438611
FujiwaraKen <fjwr1024@gmail.com>
2022-11-26 00:13:52 +0900

    add install nginx

--- main.tf
+++ main.tf
@@ -11,4 +11,13 @@ resource "aws_instance" "hello-world" {
   tags = {
     Name = "HelloWorld"
   }
+
+# nginxをインストールし、EC2を再作成
+user_data_replace_on_change = true
+
+  user_data = <<EOF
+#!/bin/bash
+amazon-linux-extras install -y nginx1.12
+systemctl start nginx
+EOF
 }
